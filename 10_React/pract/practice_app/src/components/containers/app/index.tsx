8466401b4d6a84919982629b5bc504fd7bcdf4e6
Alex Mubarakshin <muban96@mail.ru>
2020-05-13 20:19:55 +0300

    🤓 Add practice

--- 10_React/pract/practice_app/src/components/containers/app/index.tsx
+++ 10_React/pract/practice_app/src/components/containers/app/index.tsx
@@ -1,35 +1,106 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import './styles.css';
 
 import Header from '../../elements/header';
 import Footer from '../../elements/footer';
+import Card from '../../elements/card';
+import { PicsumImage } from '../../../core/model/Picsum';
 
-const App: React.FC = () => {
-  return (
-    <div className="app">
-      <Header />
+const API_URL = 'https://picsum.photos/v2/list';
+const IMAGE_LIMIT = 15;
 
-      <main className="app__main">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+type AppProps = {};
 
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </main>
+type AppState = {
+  images: PicsumImage[];
+  page: number;
 
-      <Footer />
-    </div>
-  );
-};
+  isFetching: boolean;
+  isListEnded: boolean;
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+  state: AppState = {
+    images: [],
+    page: 1,
+    isFetching: false,
+    isListEnded: false,
+  }
+
+  componentDidMount(): void {
+    window.addEventListener('scroll', this.handleScroll);
+
+    this.loadData();
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (): void => {
+    const isBottomOfPage = window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight;
+
+    if (!isBottomOfPage || this.state.isFetching || this.state.isListEnded) return;
+
+    this.setState((prevState) => ({
+      page: prevState.page + 1
+    }), this.loadData);
+  }
+
+  loadData = (): void => {
+    this.setState({
+      isFetching: true,
+    }, async () => {
+      try {
+        const response = await fetch(`${API_URL}?page=${this.state.page}&limit=${IMAGE_LIMIT}`);
+        const values = await response.json();
+
+        const isListEnded = values.length < IMAGE_LIMIT;
+
+        this.setState((prevState) => ({
+          images: prevState.images.concat(values),
+          isFetching: false,
+          isListEnded,
+        }));
+      } catch (err) {
+        console.error(err);
+
+        this.setState({
+          isFetching: false,
+        });
+      }
+    });
+
+  }
+
+  render(): React.ReactElement {
+    return (
+      <div className="app">
+        <Header />
+
+        <main className="app__main">
+          {
+            this.state.images.map((image) => (
+              <Card
+                key={image.id}
+                src={image.download_url}
+              />
+            ))
+          }
+
+          {
+            this.state.isFetching && (
+              <h4>Loading...</h4>
+            )
+          }
+        </main>
+
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ 10_React/pract/practice_app/src/core/model/Picsum.ts
@@ -0,0 +1,8 @@
+export type PicsumImage = {
+  author: string;
+  download_url: string;
+  height: number;
+  id: string;
+  url: string;
+  width: number;
+}
